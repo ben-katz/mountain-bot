7a22dacc9450e69e8cb8969f91e559f2d981653b
ben-katz <32073885+ben-katz@users.noreply.github.com>
2018-02-17 15:18:12 -0800

    create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+import asyncio
+from discord.ext import commands
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+Client = discord.Client
+client = commands.Bot(command_prefix = ".")
+description = 'RBMK Test Bot'
+
+@client.command()
+async def status(ctx):
+    await ctx.send("boop")
+
+## Initialize Bot
+client.run('NDE0NTI3OTY3MjAxMzI5MTYy.DWoq1Q.1aW-qKYa1TJFM776ECtlmQ9f9e0')
