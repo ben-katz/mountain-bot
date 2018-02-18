b401144bcaa57b9432f67924e7f840d0029c97e8
ben-katz <32073885+ben-katz@users.noreply.github.com>
2018-02-17 20:49:01 -0800

    fix caching error

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 import discord
 import asyncio
+import roads
+import pagination
+from random import getrandbits
 from discord.ext import commands
 from discord.ext.commands import Bot
 
 bot = commands.Bot(command_prefix = ".")
 description = 'RBMK Test Bot'
 
+
 @bot.command()
-async def beep(ctx):
-    await ctx.send("boop")
+async def status(ctx):
+    roadReport = roads.road_condition()
+    camSrc = "http://ns-webcams.its.sfu.ca/public/images/gaglardi-current.jpg?nocache=" + str(getrandbits(64)) + "&update=15000&timeout=1800000&offset=4"
+    e = discord.Embed(title="Burnaby Mountain Status Update", color=0xffffff, description=roadReport)
+    e.set_image(url=camSrc)
+    await ctx.send(embed=e)
+
 
 ## Initialize Bot
 bot.run('NDE0NTI3OTY3MjAxMzI5MTYy.DWoq1Q.1aW-qKYa1TJFM776ECtlmQ9f9e0')
