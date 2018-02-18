fc2265b502639179053f6cc16c2840745a170b67
bekatz <bekatz@sfu.ca>
2018-02-17 16:10:54 -0800

    fix syntax error

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-Client = discord.Client
 client = commands.Bot(command_prefix = ".")
 description = 'RBMK Test Bot'
 
