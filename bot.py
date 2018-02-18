3d2635aa5ee5e34b7f94c1bcb4498bf90677c056
ben-katz <32073885+ben-katz@users.noreply.github.com>
2018-02-17 23:44:44 -0800

    add default reactions

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import roads
+import emoji
 from random import getrandbits
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -15,7 +16,11 @@ async def status(ctx):
     camSrc = "http://ns-webcams.its.sfu.ca/public/images/gaglardi-current.jpg?nocache=" + str(getrandbits(64)) + "&update=15000&timeout=1800000&offset=4"
     e = discord.Embed(title="Burnaby Mountain Status Update", color=0xffffff, description=roadReport)
     e.set_image(url=camSrc)
-    await ctx.send(embed=e)
+    msg = await ctx.send(embed=e)
+    i = 0
+    emojis = ['\U00000031\U000020e3', '\U00000032\U000020e3', '\U00000033\U000020e3', '\U00000034\U000020e3', '\U00000035\U000020e3', '\U00000036\U000020e3']
+    for i in range(6):
+        await msg.add_reaction(emojis[i])
 
 
 ## Initialize Bot
